c1fc26bb3ccbfa957403d988a2d9e3447ccecee6
takahashi_hideki <hideo7319@gmail.com>
2025-09-28 16:55:55 +0900

    Livewireインストールとカウンターを実装

--- src/routes/web.php
+++ src/routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('index');
+})->name('index');
+
+Route::get('/counter', function () {
+    return view('counter');
+})->name('counter');
